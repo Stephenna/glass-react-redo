2798a87f05dea3763a8d0874c8a3c18725a72aa2
Stephenna J Corry <Sjcstephenna@gmail.com>
2021-02-20 23:00:30 -0500

    retrying

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-body">
+      <div className="App-main">
+        <section className="glass"></section>
+      </div>
+      <div className="circle1"></div>
+      <div className="circle2"></div>
+    </div>
+    
+  );
+}
+
+export default App;
